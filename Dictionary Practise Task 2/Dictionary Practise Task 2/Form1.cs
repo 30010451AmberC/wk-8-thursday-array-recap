c5f81ce639e6ecfed4cd7f8e2c4ad87a02d41991
Amber Cook <30010451@student.toiohomai.ac.nz>
2019-05-15 15:50:57 +1200

    Task 2 complete

---
+++ Dictionary Practise Task 2/Dictionary Practise Task 2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary_Practise_Task_2
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<int, string> dict = new Dictionary<int, string>();
+        static int counter = 1000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dict.Add(counter, textBox1.Text);
+            counter++;
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(KeyValuePair<int, string> x in dict)
+            {
+                listBox1.Items.Add($"key: {x.Key}\tValue: {x.Value}");
+            }
+        }
+    }
+}
